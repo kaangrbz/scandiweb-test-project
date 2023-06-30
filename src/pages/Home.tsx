6297f75bd8d8b567c2a7befe0e2f6c6a7ce837bf
Kaan Gürbüz <abdikaangrbz@gmail.com>
2023-07-01 02:05:54 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -73,10 +73,7 @@ const Home = () => {
   return (
     <>
       <div className="toolbar">
-        <h1>
-          Product List{' '}
-          {process.env.REACT_APP_ENDPOINT ? process.env.REACT_APP_ENDPOINT : 'undefined'}
-        </h1>
+        <h1>Product List</h1>
 
         <div className="buttons">
           <Link to="/addproduct">ADD</Link>
